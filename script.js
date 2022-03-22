6185e959f8ae83611965cd5803d3bfa560eada20
Filipe de Oliveira Ataíde <filipe.ataide@academico.ifrn.edu.br>
2022-03-22 14:52:53 -0300

    Alternando os baloes de lado.

--- script.js
+++ script.js
@@ -25,46 +25,81 @@ data = [
     }
 ]
 
-data.forEach(d => {
-    $('.container').append(
+for (let i = 0; i < data.length; i++) {
+    row = "<div class='row align-items-center' ></div>";
+    
+
+    content = `
+        
+                <div class="content rounded p-2"
+                style="border: 3px solid ` + data[i].border_color + `;">
+                    `
+        +
+        data[i].texto
+        +
         `
-        <div class="row align-items-center" >  
-            <div class="col">
-                <div class="balloon">
-                    <div class="d-flex align-items-center">
-                        <div class="content rounded p-2"
-                        style="border: 3px solid ` + d.border_color + `;">
-                            `
-                            +
-                            d.texto
-                            +
-                            `
-                        </div>
-                        <svg fill="`+ d.border_color +`" width="3.9688mm" height="5.2917mm" version="1.1" viewBox="0 0 3.9688 5.2917"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path transform="matrix(.011594 0 0 .013388 -.0033036 -.29155)"
-                                d="m0.28493 417.03-9.4e-7 -395.25 342.3 197.63z"
-                                style="paint-order:stroke fill markers" />
-                        </svg>
-                    </div>
                 </div>
-            </div>
-            <div class="col col-2">
-                <button class="label rounded-pill text-white w-100"
-                style="border: 3px solid ` + d.border_color + `;
-                background: ` + d.bg_color + `;"
-                data-color="#D4D17B" data-bg="#C9C55A">
-                    `
-                    +
-                    d.ano
-                    +
-                    `
-                </button>
-            </div>
-            <div class="col"></div>
-        </div>
+            `
+            
+    left = `
+        <svg fill="`+ data[i].border_color + `" width="3.9688mm" height="5.2917mm" version="1.1" viewBox="0 0 3.9688 5.2917"
+            xmlns="http://www.w3.org/2000/svg">
+            <path transform="matrix(.011594 0 0 .013388 -.0033036 -.29155)"
+                d="m0.28493 417.03-9.4e-7 -395.25 342.3 197.63z"
+                style="paint-order:stroke fill markers" />
+        </svg>
         `
-    )
+
+    right = `
+        <svg fill="`+ data[i].border_color + `" width="3.9688mm" height="5.2917mm" version="1.1" viewBox="0 0 3.9688 5.2917"
+        xmlns="http://www.w3.org/2000/svg">
+            <g transform="translate(1.3229)">
+                <path transform="matrix(-.011594 0 0 .013388 2.6491 -.29155)" d="m0.28493 417.03-9.4e-7 -395.25 342.3 197.63z" style="paint-order:stroke fill markers"/>
+            </g>
+        </svg>
+    `
+
+    button = `
+        <button class="label rounded-pill text-white w-100"
+        style="border: 3px solid ` + data[i].border_color + `;
+        background: ` + data[i].bg_color + `;"
+        data-color="#D4D17B" data-bg="#C9C55A">
+        `
+        +
+        data[i].ano
+        +
+        `
+        </button>
+        `
+
+
+    if ((i % 2) == 0) {
+        balloon = "<div class='balloon'><div class='d-flex align-items-center'>"
+                    + content + left + "</div></div>"
+        $('.container').append(
+            "<div class='row align-items-center' ><div class='col'>"
+            + balloon + "</div><div class='col col-2'>"
+            + button + "</div><div class='col'></div></div>"
+        )
+    }
+    else {
+        balloon = "<div class='balloon'><div class='d-flex align-items-center'>"
+                    + right + content + "</div></div>"
+        $('.container').append(
+            "<div class='row align-items-center' ><div class='col'></div><div class='col col-2'>"
+            + button + "</div><div class='col'>"
+            + balloon + "</div></div>"
+        )
+    }
+
+}
+
+data.forEach(d => {
+
+
+
+
+
 });
 
 $('.balloon').hide();
